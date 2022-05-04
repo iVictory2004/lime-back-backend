a8268b22190bccd3ec84391cbb6a726bb81fc549
Viktorashi <33178074+iVictory2004@users.noreply.github.com>
2022-05-04 13:57:32 +0300

    update

--- api/getBikes.js
+++ api/getBikes.js
@@ -1,19 +1,5 @@
-const express = require( 'express' )
-const {
-    getClosestBikes
-} = require( '../exec/getZonesAndBikes' )
-const router = express.Router()
+// const router = express.Router()
 
-router.get( '/', async ( req, res ) => {
-    console.log( `Uite coordonatele la un prost sa-i dai ddos:` )
-    console.log( req.query )
-    try {
-        const bikes = await getClosestBikes( req.query.lat, req.query.lng )
-        console.log( bikes )
-        res.json( bikes )
-    } catch ( e ) {
-        console.log( e )
-    }
-} )
+// router.get( '/', )
 
-module.exports = router
+// module.exports = router
