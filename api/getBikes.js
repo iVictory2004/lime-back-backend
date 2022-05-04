bd0e564294fe74c2f3701dbfc80c9bd7d75ae0b4
Viktorashi <33178074+iVictory2004@users.noreply.github.com>
2022-05-04 14:37:14 +0300

    added cors even better

--- api/getBikes.js
+++ api/getBikes.js
@@ -1,5 +1,39 @@
-// const router = express.Router()
+const express = require( 'express' )
+const router = express.Router()
+const {
+    getClosestBikes
+} = require( '../exec/getZonesAndBikes' )
 
-// router.get( '/', )
+const allowCors = fn => async ( req, res ) => {
+    res.setHeader( 'Access-Control-Allow-Credentials', true )
+    res.setHeader( 'Access-Control-Allow-Origin', '*' )
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader( 'Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT' )
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if ( req.method === 'OPTIONS' ) {
+        res.status( 200 ).end()
+        return
+    }
+    return await fn( req, res )
+}
 
-// module.exports = router
+const handler = async ( req, res ) => {
+    console.log( `Uite coordonatele la un prost sa-i dai ddos:` )
+    console.log( req.query )
+    if ( req.query.lat && req.query.lng )
+        try {
+            const bikes = await getClosestBikes( req.query.lat, req.query.lng )
+            console.log( bikes )
+            res.json( bikes )
+        } catch ( e ) {
+            console.log( e )
+        }
+    else
+        res.send( `N-ai trimis parametrii bine bruh` )
+}
+
+module.exports = allowCors( handler )
--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 const express = require( 'express' )
 const app = express()
-// const bikes = require( './api/getBikes' )
+const bikes = require( './api/getBikes' )
 const PORT = process.env.PORT || 3000
-const {
-    getClosestBikes
-} = require( './exec/getZonesAndBikes' )
 
+app.use( '/', bikes )
 
-app.use( '/', async ( req, res ) => {
-    console.log( `Uite coordonatele la un prost sa-i dai ddos:` )
-    console.log( req.query )
-    if ( req.query.lat && req.query.lng )
-        try {
-            const bikes = await getClosestBikes( req.query.lat, req.query.lng )
-            console.log( bikes )
-            res.json( bikes )
-        } catch ( e ) {
-            console.log( e )
-        }
-    else
-        res.send( `N-ai trimis parametrii bine bruh` )
-} )
 
 app.listen( PORT, () => {
-    console.log( `E PORTNIT PORT ${PORT}` )
+    console.log( `E PORTNIT PORT  coi ${PORT}` )
 } )
